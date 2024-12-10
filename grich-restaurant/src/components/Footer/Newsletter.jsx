32df1ef2f6af2de400ae57334aa5cde4d23a4587
Anjali Methre <sonaanjali2211@gmail.com>
2024-12-10 17:11:59 +0530

    hello

--- grich-restaurant/src/components/Footer/Newsletter.jsx
+++ grich-restaurant/src/components/Footer/Newsletter.jsx
@@ -7,13 +7,15 @@ const Newsletter = () => (
   <div className="app__newsletter">
     <div className="app__newsletter-heading">
       <SubHeading title="Newsletter" />
-      <h1 className="headtext__cormorant">Subscribe to our Newsletter</h1>
-      <p className="p__opensans">And never miss Latest Updates</p>
+      <h1 className="headtext cormorant">Subscribe To Our Newsletter</h1>
+      <p className="p__opensans">And Never miss the latest updates</p>
     </div>
+
     <div className="app__newsletter-input flex__center">
-      <input type="email" placeholder="Enter Your Emial Address" />
-      <button className="Custom__button">Subscribe</button>
+      <input type="email" placeholder="enter ur email address" />
+      <button className="custom__button">Subscribe</button>
     </div>
+    
   </div>
 );
 
